d3691127a36da7488b8e950a21311bd95fa6fb93
Ali Ozkan <aliozkan4334@gmail.com>
2024-01-04 05:25:29 +0100

    footer

--- rc02-comp-jsx/src/App.js
+++ rc02-comp-jsx/src/App.js
@@ -1,7 +1,13 @@
+import Footer from "./Footer"
+import Header from "./Header"
+
 function App() {
+  console.log("deneme");
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <Header/>
+      <Footer/>
     </div>
   )
 }
