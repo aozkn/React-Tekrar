dac5c256dd516de68b134d409c72100beaf1dae2
Ali Ozkan <aliozkan4334@gmail.com>
2024-01-04 05:31:10 +0100

    content

--- rc02-comp-jsx/src/App.js
+++ rc02-comp-jsx/src/App.js
@@ -1,15 +1,17 @@
-import Footer from "./Footer"
-import Header from "./Header"
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   console.log("deneme");
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <Header/>
-      <Footer/>
+      <Header />
+      <Content />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ rc02-comp-jsx/src/Content.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <main>
+      <h2>Lorem ipsum dolor sit.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellat
+        voluptates consectetur totam, voluptas provident quisquam cum incidunt
+        fugit enim.
+      </p>
+      
+    </main>
+  );
+};
+
+export default Content;
