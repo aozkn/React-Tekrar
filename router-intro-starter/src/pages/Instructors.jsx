f477385d3bd53a35502da7727f363245d5b7b877
Ali Ozkan <aliozkan4334@gmail.com>
2024-01-21 10:24:27 +0100

    feat:useLocation

--- router-intro-starter/src/pages/Instructors.jsx
+++ router-intro-starter/src/pages/Instructors.jsx
@@ -6,10 +6,17 @@ const Instructors = () => {
       <h1>Instructors</h1>
       <div className="d-flex flex-column">
         {/* //!Absulote path (tam adres vermek) */}
-        <Link to={"/instructors/1"}>Instructor1</Link>
+        <Link to={"/instructors/1"} state={{ name: "ali" }}>
+          Instructor1
+        </Link>
         {/* //!Relative Path (goreceli adres vermek) */}
-        <Link to="2">Instructor2</Link>
-        <Link to={"3"}> Instructor3</Link>
+        <Link to="2" state={{ name: "sami" }}>
+          Instructor2
+        </Link>
+        <Link to={"3"} state={{ name: "salih" }}>
+          {" "}
+          Instructor3
+        </Link>
       </div>
     </div>
   );
--- router-intro-starter/src/pages/InstructorsDetail.jsx
+++ router-intro-starter/src/pages/InstructorsDetail.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useLocation, useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 const InstructorsDetail = () => {
-  return <div className="p-3">InstructorsDetail</div>;
+  //   let param = useParams();
+  //* Linkteki parametreyi almak icin useParams Hook'u kullanilabilir.
+  //* yakalanan id'ye ait güncel veriler fetch yapılabilir
+  let { id } = useParams();
+  //* navigate ile gonderilen state'i yakalamak icin useLocation Hook'u kullanilabilir.
+  //* Bu durumda veri, state ile geldigi icin yeniden fetch yapilmasina gerek kalmaz
+
+  console.log(id);
+  //   console.log(param);
+
+  let { state, pathname } = useLocation();
+  console.log({ pathname });
+  console.log({ state });
+  return (
+    <div className="p-3">
+      <p>
+        From useParam: I'm Instructor{" "}
+        <span className="text-warning fw-bolder">{id}</span>
+      </p>
+      <p>
+        From useLocation: I'm Instructor{" "}
+        <span className="text-warning fw-bolder">{state?.name}</span>
+      </p>
+      <p>
+        Our path is : <span className="text-warning fw-bolder">{pathname}</span>
+      </p>
+    </div>
+  );
 };
 
 export default InstructorsDetail;
