76e23808a19b81fd8e89c9339324459090155f51
Ali Ozkan <aliozkan4334@gmail.com>
2024-01-30 05:27:27 +0100

    k

---
+++ styled-intro/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button.styled";
+
+function App() {
+  return (
+    <>
+      <h1>Styled-Components</h1>
+      <Button>Click On</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ styled-intro/src/components/Button.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #25aa58;
+  color: white;
+  border-radius: 20%;
+  margin: 1rem;
+  border: 2px solid hotpink;
+  padding: 0.7rem 3rem;
+  font-size: 1.3 rem;
+`;
