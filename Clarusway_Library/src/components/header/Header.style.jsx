0d8f12c1c6ef5e289593ec5e024c6a5b7ba9a845
Ali Ozkan <aliozkan4334@gmail.com>
2024-02-05 12:59:49 +0100

    feat:header

--- Clarusway_Library/src/components/header/Header.style.jsx
+++ Clarusway_Library/src/components/header/Header.style.jsx
@@ -1,1 +1,14 @@
 // Header Style
+import styled from "styled-components";
+import Flex from "../../styles/Flex";
+
+export const HeaderContainer = styled(Flex)`
+  flex-direction: column;
+`;
+export const HeaderTitle = styled.h1``;
+
+export const HeaderForm =styled.form``;
+
+export const SearchInput =styled.input``;
+export const SelectBox = styled.search``;
+export const HeaderButton= styled.button``;
--- Clarusway_Library/src/pages/home/Home.jsx
+++ Clarusway_Library/src/pages/home/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "../../components/header/Header";
+import Card from "../../components/card/Card";
+import { CardContainer, HomeContainer, HomeImage } from "./Home.style";
+import HomeImg from "../../assets/books.jpg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <HomeContainer>
+      <Header />
+      <HomeImage>
+        <img src={HomeImg} alt="books" />
+      </HomeImage>
+      <CardContainer>
+        <Card />
+      </CardContainer>
+    </HomeContainer>
+  );
 };
 
 export default Home;
