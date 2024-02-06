1d0385da556a2fd07c25ea77ee9553530beb086a
Ali Ozkan <aliozkan4334@gmail.com>
2024-02-06 20:01:46 +0100

    feat:data fetchted from api and used map to show cards

--- Clarusway_Library/src/pages/home/Home.jsx
+++ Clarusway_Library/src/pages/home/Home.jsx
@@ -3,17 +3,24 @@ import Header from "../../components/header/Header";
 import Card from "../../components/card/Card";
 import { CardContainer, HomeContainer, HomeImage } from "./Home.style";
 import HomeImg from "../../assets/books.jpg";
+import { useBooksContext } from "../../context/BooksContext";
 
 const Home = () => {
+  const { bookData } = useBooksContext();
   return (
     <HomeContainer>
       <Header />
-      <HomeImage>
-        <img src={HomeImg} alt="books" />
-      </HomeImage>
-      <CardContainer>
-        <Card />
-      </CardContainer>
+      {bookData.length ? (
+        <CardContainer>
+          {bookData.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </CardContainer>
+      ) : (
+        <HomeImage>
+          <img src={HomeImg} alt="books" />
+        </HomeImage>
+      )}
     </HomeContainer>
   );
 };
