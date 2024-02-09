0d74b1d67fc26e057ddf81976003ed568fad2554
Ali Ozkan <aliozkan4334@gmail.com>
2024-02-09 07:18:32 +0100

    feat: signIn  context writed

--- movie-app/src/context/AuthContext.jsx
+++ movie-app/src/context/AuthContext.jsx
@@ -1,6 +1,9 @@
 import React, { createContext } from "react";
 import { auth } from "../auth/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 export const AuthContext = createContext();
 
 //* with custom hook
@@ -15,10 +18,23 @@ const AuthContextProvider = ({ children }) => {
       let userC = await createUserWithEmailAndPassword(auth, email, password);
       console.log(userC);
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
     }
   };
-  const values = { createUser };
+
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+  const signIn = async (email, password) => {
+    try {
+      //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const values = { createUser, signIn };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
