25d2b3265d74c63de3ff317fa9c18a57c7710fad
Ali Ozkan <aliozkan4334@gmail.com>
2024-02-09 03:27:07 +0100

    added starter for movie app

--- Tailwinds Css/src/App.js
+++ Tailwinds Css/src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-center ">Hello world!</h1>
+    <div className=" bg-green-600 w-34">
+      <h1 className="text-3xl font-bold text-center  underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
--- Tailwinds Css/src/index.js
+++ Tailwinds Css/src/index.js
@@ -1,9 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-const root = ReactDOM.createRoot(document.getElementById("root"))
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../src/index.css";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
